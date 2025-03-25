60c3784585fb15c80895c5ff944b7d39fdf643d2
shamal <shamalmelantha3@gmail.com>
2025-03-25 10:05:18 +0530

    update

---
+++ src/redux/user/userSilce.js
@@ -0,0 +1,57 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  currentUser: null,
+  loading: false,
+  error: false,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    signInStart: (state) => {
+      state.loading = true;
+    },
+    signInSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = false;
+    },
+    signInFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+   
+    signoutSuccess: (state) => {
+      state.currentUser = null;
+      state.error = null;
+      state.loading = false;
+    },
+    updateSart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    updateSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    updateFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+  },
+});
+
+export const {
+  signInFailure,
+  signInStart,
+  signInSuccess,
+  signoutSuccess,
+  updateFailure,
+  updateSart,
+  updateSuccess,
+} = userSlice.actions;
+
+export default userSlice.reducer;
